64aec0f72c221134e34a9b27da2d3fff9345518b
santiagoperez <santiago.perez.chavez@gmail.com>
2013-06-09 12:18:34 +0200

    Best tree

--- src/XMLParser.java
+++ src/XMLParser.java
@@ -4,6 +4,25 @@ import java.io.File;
 public class XMLParser {
 
 	File file;
+	
+	XMLParser(File myFile) {
+		file = myFile;
+	}
+	
+	XMLParser() {
+		file = new File("settings.xml");
+	}
+	
+	// run
+	public void go() {
+		//open file
+		//start parsing
+			//read line, if <> new class (have to use split maybe)
+			//assign values
+			// if </> go out from class
+	}
+	
+	
 	/**
 	 * @param args
 	 */
@@ -11,6 +30,7 @@ public class XMLParser {
 		// TODO Auto-generated method stub
 
 		XMLParser parser = new XMLParser();
+		parser.go();
 	}
 
 }
