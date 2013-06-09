0b0a2735ff8934afcdc310230d77e4359bdd8fe6
santiagoperez <santiago.perez.chavez@gmail.com>
2013-06-09 13:06:55 +0200

    Reader implemented

--- src/XMLParser.java
+++ src/XMLParser.java
@@ -1,20 +1,41 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 
 
 public class XMLParser {
 
 	File file;
 	
-	XMLParser(File myFile) {
-		file = myFile;
+	XMLParser(String myFile) {
+		file = new File(myFile);
 	}
 	
 	XMLParser() {
 		file = new File("settings.xml");
 	}
 	
+	public String readFile() {
+		String xml = null;
+		String line = null;
+		try  {
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+			xml = reader.readLine();
+			while ((line = reader.readLine()) != null) {
+				xml = xml + "\n" + line;
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		return xml;
+	}
+	
+	
 	// run
 	public void go() {
+		String xml = readFile();
+		System.out.println(xml);
 		//open file
 		//start parsing
 			//read line, if <> new class (have to use split maybe)
@@ -29,7 +50,7 @@ public class XMLParser {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		XMLParser parser = new XMLParser();
+		XMLParser parser = new XMLParser("settings2.xml");
 		parser.go();
 	}
 
