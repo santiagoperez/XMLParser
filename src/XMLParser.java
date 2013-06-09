95a94d0b190dae4acd376fe9abd84e24ce8a7146
santiagoperez <santiago.perez.chavez@gmail.com>
2013-06-09 19:00:33 +0200

    First version working (only plot, not creating classes!)

--- src/XMLParser.java
+++ src/XMLParser.java
@@ -1,12 +1,15 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 
 public class XMLParser {
 
 	File file;
 	String xml;
+	ArrayList<String> fields = new ArrayList<String>();
+	//int indexTextParse = 1;
 	
 	XMLParser(String myFile) {
 		file = new File(myFile);
@@ -32,44 +35,72 @@ public class XMLParser {
 		return xml;
 	}
 	
-	public void parse(String part) {
+	// Deletes the chain text in the global XML
+	public void deleteInXML(String text) {
+		xml = xml.substring(0, xml.indexOf(text)) + xml.substring(xml.indexOf(text)+text.length(),xml.length());
+	}
+	
+	public void parse(String parent, String part) {
 		String finalLine = "</";
 		
-		int initNameClass = 1 + part.indexOf('<');
-		if (initNameClass == 0) return;
-		int endNameClass = part.indexOf('>');
-		String classParsed = part.substring(initNameClass,endNameClass);
+		if (part.indexOf("<") == -1) {
+			String chainToDelete = "<"+parent+">"+ part + "</"+parent+">";
+			deleteInXML(chainToDelete);
+			return;
+		}
 		
-		int initValueClass = 1 + endNameClass;
-		int endValueClass = part.indexOf("</" + classParsed + ">");
-		String inside = part.substring(initValueClass,endValueClass);
-		
-		System.out.println("Class: " + classParsed);
-		System.out.println("Inside: " + inside);
-		parse(inside);
+			int initNameClass = 1+part.indexOf('<');
+			int endNameClass = part.indexOf('>');
+			String classParsed = part.substring(initNameClass,endNameClass);
+			
+			int initValueClass = 1 + endNameClass;
+			int endValueClass = part.indexOf("</" + classParsed + ">");
+			String inside = part.substring(initValueClass,endValueClass);
+			if (!fields.contains(classParsed)) {
+				fields.add(classParsed);
+				System.out.println("Class: " + classParsed);
+				System.out.println("Inside: " + inside);
+				System.out.println("Parent: " + parent);
+				System.out.println();
+			}
+			parent = classParsed;
+			parse(parent,inside);
 		
 	}
 	
-	public void spacesOut() {
+	public void spacesAndEntersOut() {
 		
 		String prov = "" + xml.charAt(0);
+		
+		// Out spaces
 		for (int i = 1; i < xml.length(); i++) {
 			if ((prov.charAt(prov.length()-1) == '\n') && (xml.charAt(i) == ' ')) {
 			} else {
 				prov += xml.charAt(i);
 			}
-			
+		}
+		
+		xml = prov;
+		prov = "" + xml.charAt(0);
+		
+		// Out enters. COMBINE IN THE SAME LOOP!!!
+		for (int i = 1; i < xml.length(); i++) {
+			if (xml.charAt(i) == '\n') {
+			} else {
+				prov += xml.charAt(i);
+			}
 		}
 		xml = prov;
-		
 	}
 	
 	// run
 	public void go() {
 		xml = readFile();
-		spacesOut();
+		spacesAndEntersOut();
 		System.out.println(xml);
-		parse(xml);
+		while(xml.length() > 0) {
+			parse("",xml);
+		}
 		/*
 		 * spaces out
 		 * 
